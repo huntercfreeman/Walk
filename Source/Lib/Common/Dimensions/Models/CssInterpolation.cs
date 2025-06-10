c2a461d2ad4c67b6c87a7b20c62f706a71271768
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-06-10 12:16:33 -0400

    ToString changes

--- Source/Lib/Common/Dimensions/Models/CssInterpolation.cs
+++ Source/Lib/Common/Dimensions/Models/CssInterpolation.cs
@@ -8,6 +8,20 @@ namespace Walk.Common.RazorLib.Dimensions.Models;
 /// when being rendered.
 /// <br/><br/>
 /// So, this file ensures period formatting regardless of localization.
+///
+/// WARNING: In hot paths numbers will have ToString() invoked directly,...
+/// ...rather than ToCssValue();
+/// 
+/// In non-hot paths ToCssValue is still used on int values
+/// in the chance that someone changes the int to a double,
+/// or some other number that will require InvariantCulture.
+///
+/// Therefore, be wary about changing int values to doubles or etc...
+/// because depending on how hot the path is, there might actually
+/// be direct ToString() invocations.
+///
+/// I need to look into [MethodImpl(MethodImplOptions.AggressiveInlining)]
+/// and under what situations the compiler will inline for you.
 /// </summary>
 public static class CssInterpolation
 {
@@ -20,6 +34,21 @@ public static class CssInterpolation
     public static string ToCssValue(this float value) =>
         value.ToString(System.Globalization.CultureInfo.InvariantCulture);
 
+    /// <summary>
+    /// WARNING: In hot paths numbers will have ToString() invoked directly,...
+    /// ...rather than ToCssValue();
+    /// 
+    /// In non-hot paths ToCssValue is still used on int values
+    /// in the chance that someone changes the int to a double,
+    /// or some other number that will require InvariantCulture.
+    ///
+    /// Therefore, be wary about changing int values to doubles or etc...
+    /// because depending on how hot the path is, there might actually
+    /// be direct ToString() invocations.
+    ///
+    /// I need to look into [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    /// and under what situations the compiler will inline for you.
+    /// </summary>
     public static string ToCssValue(this int value) =>
         value.ToString(System.Globalization.CultureInfo.InvariantCulture);
 }
