562a044781c8cc4dd21b6aebdb84742a6f3ce254
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-06-20 14:56:23 -0400

    Separate the DragInitializer

---
+++ Source/Lib/Common/Drags/Displays/DragInitializer.razor.cs
@@ -0,0 +1,110 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Walk.Common.RazorLib.Dynamics.Models;
+using Walk.Common.RazorLib.Reactives.Models;
+using Walk.Common.RazorLib.Drags.Models;
+using Walk.Common.RazorLib.Keys.Models;
+
+namespace Walk.Common.RazorLib.Drags.Displays;
+
+public partial class DragInitializer : ComponentBase, IDisposable
+{
+    [Inject]
+    private IDragService DragService { get; set; } = null!;
+    
+    /* Start DragInitializer */
+    private string StyleCss => DragService.GetDragState().ShouldDisplay
+        ? string.Empty
+        : "display: none;";
+
+    private ThrottleOptimized<MouseEvent> _throttle;
+    
+    public struct MouseEvent
+    {
+    	public MouseEvent(bool isOnMouseMove, MouseEventArgs mouseEventArgs)
+    	{
+    		IsOnMouseMove = isOnMouseMove;
+    		MouseEventArgs = mouseEventArgs;
+    	}
+    
+    	public bool IsOnMouseMove { get; }
+    	public MouseEventArgs MouseEventArgs { get; }
+    }
+
+    private IDropzone? _onMouseOverDropzone = null;
+    /* End DragInitializer */
+    
+    protected override void OnInitialized()
+    {
+        _throttle = new(ThrottleFacts.TwentyFour_Frames_Per_Second, async (args, _) =>
+	    {
+	    	if (args.IsOnMouseMove)
+	    	{
+	    		if ((args.MouseEventArgs.Buttons & 1) != 1)
+	                DRAG_DispatchClearDragStateAction();
+	            else
+	                DragService.ReduceShouldDisplayAndMouseEventArgsSetAction(true, args.MouseEventArgs);
+	
+	            return;
+	    	}
+	    	else
+	    	{
+	    		var dragState = DragService.GetDragState();
+				var localOnMouseOverDropzone = _onMouseOverDropzone;
+	    	
+	    		DRAG_DispatchClearDragStateAction();
+	
+	            var draggableViewModel = dragState.Drag;
+	            if (draggableViewModel is not null)
+	            {
+	                await draggableViewModel
+	                    .OnDragEndAsync(args.MouseEventArgs, localOnMouseOverDropzone)
+	                    .ConfigureAwait(false);
+	            }
+	    	}
+	    });
+    
+        DragService.DragStateChanged += OnDragStateChanged;
+    }
+    
+    /* Start DragInitializer */
+    private async void OnDragStateChanged()
+    {
+    	await InvokeAsync(StateHasChanged);
+    }
+    /* End DragInitializer */
+    
+    private void DRAG_DispatchClearDragStateAction()
+    {
+		_onMouseOverDropzone = null;
+		
+        DragService.ReduceShouldDisplayAndMouseEventArgsAndDragSetAction(
+        	false,
+            null,
+			null);
+    }
+
+    private void DRAG_DispatchSetDragStateActionOnMouseMove(MouseEventArgs mouseEventArgs)
+    {
+        _throttle.Run(new(isOnMouseMove: true, mouseEventArgs));
+    }
+
+    private void DRAG_DispatchSetDragStateActionOnMouseUp(MouseEventArgs mouseEventArgs)
+    {
+        _throttle.Run(new(isOnMouseMove: false, mouseEventArgs));
+    }
+
+	private string DRAG_GetIsActiveCssClass(IDropzone dropzone)
+	{
+		var onMouseOverDropzoneKey = _onMouseOverDropzone?.DropzoneKey ?? Key<IDropzone>.Empty;
+
+		return onMouseOverDropzoneKey == dropzone.DropzoneKey
+            ? "di_active"
+			: string.Empty;
+	}
+    
+    public void Dispose()
+    {
+        DragService.DragStateChanged -= OnDragStateChanged;
+    }
+}
--- Source/Lib/Common/Installations/Displays/WalkCommonInitializer.razor.cs
+++ Source/Lib/Common/Installations/Displays/WalkCommonInitializer.razor.cs
@@ -6,14 +6,6 @@ using Walk.Common.RazorLib.Contexts.Models;
 using Walk.Common.RazorLib.Dimensions.Models;
 
 
-/* Start DragInitializer */
-using Microsoft.AspNetCore.Components.Web;
-using Walk.Common.RazorLib.Dynamics.Models;
-using Walk.Common.RazorLib.Reactives.Models;
-using Walk.Common.RazorLib.Drags.Models;
-/* End DragInitializer */
-
-
 /* Start DialogInitializer */
 using Walk.Common.RazorLib.Dialogs.Models;
 using Walk.Common.RazorLib.Contexts.Displays;
@@ -63,6 +55,9 @@ namespace Walk.Common.RazorLib.Installations.Displays;
 /// Ensure only 1 instance is rendered
 /// to avoid race condition with:
 /// 'BackgroundTaskService.ContinuousTaskWorker.StartAsyncTask = Task.Run...'
+///
+/// <DragInitializer/> is a separate component and must be rendered prior to this one
+/// to get the drag functionality.
 /// </summary>
 public partial class WalkCommonInitializer : ComponentBase, IDisposable
 {
@@ -74,12 +69,6 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
     private BrowserResizeInterop BrowserResizeInterop { get; set; } = null!;
     [Inject]
     private WalkHostingInformation WalkHostingInformation { get; set; } = null!;
-    
-    
-    /* Start DragInitializer */
-    [Inject]
-    private IDragService DragService { get; set; } = null!;
-    /* End DragInitializer */
     
     
     /* Start DialogInitializer */
@@ -123,29 +112,6 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
     private CancellationTokenSource _workerCancellationTokenSource = new();
     
     
-    /* Start DragInitializer */
-    private string StyleCss => DragService.GetDragState().ShouldDisplay
-        ? string.Empty
-        : "display: none;";
-
-    private ThrottleOptimized<MouseEvent> _throttle;
-    
-    public struct MouseEvent
-    {
-    	public MouseEvent(bool isOnMouseMove, MouseEventArgs mouseEventArgs)
-    	{
-    		IsOnMouseMove = isOnMouseMove;
-    		MouseEventArgs = mouseEventArgs;
-    	}
-    
-    	public bool IsOnMouseMove { get; }
-    	public MouseEventArgs MouseEventArgs { get; }
-    }
-
-    private IDropzone? _onMouseOverDropzone = null;
-    /* End DragInitializer */
-    
-    
     /* Start DialogInitializer */
     private ContextBoundary? _dialogContextBoundary;
     /* End DialogInitializer */
@@ -170,35 +136,6 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
     
 	protected override void OnInitialized()
 	{
-	    _throttle = new(ThrottleFacts.TwentyFour_Frames_Per_Second, async (args, _) =>
-	    {
-	    	if (args.IsOnMouseMove)
-	    	{
-	    		if ((args.MouseEventArgs.Buttons & 1) != 1)
-	                DRAG_DispatchClearDragStateAction();
-	            else
-	                DragService.ReduceShouldDisplayAndMouseEventArgsSetAction(true, args.MouseEventArgs);
-	
-	            return;
-	    	}
-	    	else
-	    	{
-	    		var dragState = DragService.GetDragState();
-				var localOnMouseOverDropzone = _onMouseOverDropzone;
-	    	
-	    		DRAG_DispatchClearDragStateAction();
-	
-	            var draggableViewModel = dragState.Drag;
-	            if (draggableViewModel is not null)
-	            {
-	                await draggableViewModel
-	                    .OnDragEndAsync(args.MouseEventArgs, localOnMouseOverDropzone)
-	                    .ConfigureAwait(false);
-	            }
-	    	}
-	    });
-    
-        DragService.DragStateChanged += OnDragStateChanged;
     	DialogService.DialogStateChanged += OnDialogStateChanged;
     	WidgetService.WidgetStateChanged += OnWidgetStateChanged;
     	NotificationService.NotificationStateChanged += OnNotificationStateChanged;
@@ -286,47 +223,6 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
 	}
     
     
-    /// <summary>
-    /// Presumptions:
-	/// - Dispose is invoked from UI thread
-	/// - Dispose being ran stops the Blazor lifecycle from being ran in the future
-    ///     - i.e.: OnInitialized(), Dispose(), OnAfterRender() <---- bad. Is it possible?
-    /// </summary>
-    public void Dispose()
-    {
-    	BrowserResizeInterop.DisposeWindowSizeChanged(CommonBackgroundTaskApi.JsRuntimeCommonApi);
-    	
-    	_workerCancellationTokenSource.Cancel();
-    	_workerCancellationTokenSource.Dispose();
-    	
-    	BackgroundTaskService.ContinuousWorker.StartAsyncTask = null;
-    	BackgroundTaskService.IndefiniteWorker.StartAsyncTask = null;
-    	
-    	DragService.DragStateChanged -= OnDragStateChanged;
-		DialogService.DialogStateChanged -= OnDialogStateChanged;
-    	WidgetService.WidgetStateChanged -= OnWidgetStateChanged;
-    	NotificationService.NotificationStateChanged -= OnNotificationStateChanged;
-        DropdownService.DropdownStateChanged -= OnDropdownStateChanged;
-    	OutlineService.OutlineStateChanged -= OnOutlineStateChanged;
-		TooltipService.TooltipStateChanged -= OnTooltipStateChanged;
-		
-		var notificationState = NotificationService.GetNotificationState();
-
-        foreach (var notification in notificationState.DefaultList)
-        {
-            NotificationService.ReduceDisposeAction(notification.DynamicViewModelKey);
-        }
-    }
-    
-    
-    /* Start DragInitializer */
-    private async void OnDragStateChanged()
-    {
-    	await InvokeAsync(StateHasChanged);
-    }
-    /* End DragInitializer */
-    
-    
     /* Start DialogInitializer */
     private Task HandleOnFocusIn()
     {
@@ -391,36 +287,7 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
     
     
     /* Start of misc */
-    private void DRAG_DispatchClearDragStateAction()
-    {
-		_onMouseOverDropzone = null;
-		
-        DragService.ReduceShouldDisplayAndMouseEventArgsAndDragSetAction(
-        	false,
-            null,
-			null);
-    }
-
-    private void DRAG_DispatchSetDragStateActionOnMouseMove(MouseEventArgs mouseEventArgs)
-    {
-        _throttle.Run(new(isOnMouseMove: true, mouseEventArgs));
-    }
-
-    private void DRAG_DispatchSetDragStateActionOnMouseUp(MouseEventArgs mouseEventArgs)
-    {
-        _throttle.Run(new(isOnMouseMove: false, mouseEventArgs));
-    }
-
-	private string DRAG_GetIsActiveCssClass(IDropzone dropzone)
-	{
-		var onMouseOverDropzoneKey = _onMouseOverDropzone?.DropzoneKey ?? Key<IDropzone>.Empty;
-
-		return onMouseOverDropzoneKey == dropzone.DropzoneKey
-            ? "di_active"
-			: string.Empty;
-	}
-	
-	private Task WIDGET_RemoveWidget()
+    private Task WIDGET_RemoveWidget()
     {
     	WidgetService.SetWidget(null);
     	return Task.CompletedTask;
@@ -525,4 +392,36 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
 		return styleBuilder.ToString();
 	}
     /* End of misc */
+    
+    
+    /// <summary>
+    /// Presumptions:
+	/// - Dispose is invoked from UI thread
+	/// - Dispose being ran stops the Blazor lifecycle from being ran in the future
+    ///     - i.e.: OnInitialized(), Dispose(), OnAfterRender() <---- bad. Is it possible?
+    /// </summary>
+    public void Dispose()
+    {
+    	BrowserResizeInterop.DisposeWindowSizeChanged(CommonBackgroundTaskApi.JsRuntimeCommonApi);
+    	
+    	_workerCancellationTokenSource.Cancel();
+    	_workerCancellationTokenSource.Dispose();
+    	
+    	BackgroundTaskService.ContinuousWorker.StartAsyncTask = null;
+    	BackgroundTaskService.IndefiniteWorker.StartAsyncTask = null;
+    	
+		DialogService.DialogStateChanged -= OnDialogStateChanged;
+    	WidgetService.WidgetStateChanged -= OnWidgetStateChanged;
+    	NotificationService.NotificationStateChanged -= OnNotificationStateChanged;
+        DropdownService.DropdownStateChanged -= OnDropdownStateChanged;
+    	OutlineService.OutlineStateChanged -= OnOutlineStateChanged;
+		TooltipService.TooltipStateChanged -= OnTooltipStateChanged;
+		
+		var notificationState = NotificationService.GetNotificationState();
+
+        foreach (var notification in notificationState.DefaultList)
+        {
+            NotificationService.ReduceDisposeAction(notification.DynamicViewModelKey);
+        }
+    }
 }
