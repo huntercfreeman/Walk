29b7945136cc2f3a939b5e703e3ba9cbd6ce6448
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-06-20 14:15:21 -0400

    Misc

--- Source/Lib/Common/Installations/Displays/WalkCommonInitializer.razor.cs
+++ Source/Lib/Common/Installations/Displays/WalkCommonInitializer.razor.cs
@@ -508,186 +508,139 @@ public partial class WalkCommonInitializer : ComponentBase, IDisposable
 	}
     /* End TooltipInitializer */
     
-    
-    
-    /* Start DragInitializer */
-    public partial class DragInitializer : ComponentBase, IDisposable
+    /* Start of misc */
+    private void DRAG_DispatchClearDragStateAction()
     {
-        private void DispatchClearDragStateAction()
-        {
-    		_onMouseOverDropzone = null;
-    		
-            DragService.ReduceShouldDisplayAndMouseEventArgsAndDragSetAction(
-            	false,
-                null,
-    			null);
-        }
-    
-        private void DispatchSetDragStateActionOnMouseMove(MouseEventArgs mouseEventArgs)
-        {
-            _throttle.Run(new(isOnMouseMove: true, mouseEventArgs));
-        }
-    
-        private void DispatchSetDragStateActionOnMouseUp(MouseEventArgs mouseEventArgs)
-        {
-            _throttle.Run(new(isOnMouseMove: false, mouseEventArgs));
-        }
-    
-    	private string GetIsActiveCssClass(IDropzone dropzone)
-    	{
-    		var onMouseOverDropzoneKey = _onMouseOverDropzone?.DropzoneKey ?? Key<IDropzone>.Empty;
-    
-    		return onMouseOverDropzoneKey == dropzone.DropzoneKey
-                ? "di_active"
-    			: string.Empty;
-    	}
+		_onMouseOverDropzone = null;
+		
+        DragService.ReduceShouldDisplayAndMouseEventArgsAndDragSetAction(
+        	false,
+            null,
+			null);
     }
-    /* End DragInitializer */
-    
-    
-    
-    /* Start DialogInitializer */
-    public partial class DialogInitializer : ComponentBase, IDisposable { }
-    /* End DialogInitializer */
-    
-    
-    
-    /* Start WidgetInitializer */
-    public partial class WidgetInitializer : ComponentBase, IDisposable
-    {
-    	private Task RemoveWidget()
-        {
-        	WidgetService.SetWidget(null);
-        	return Task.CompletedTask;
-        }
-    }
-    /* End WidgetInitializer */
-    
-    
-    
-    /* Start NotificationInitializer */
-    public partial class NotificationInitializer : ComponentBase, IDisposable { }
-    /* End NotificationInitializer */
-    
-    
-    
-    /* Start DropdownInitializer */
-    public partial class DropdownInitializer : ComponentBase, IDisposable
-    {
-    	private async Task ClearActiveKeyList()
-        {
-        	var firstDropdown = DropdownService.GetDropdownState().DropdownList.FirstOrDefault();
-        	
-        	if (firstDropdown is not null)
-        	{
-        		var restoreFocusOnCloseFunc = firstDropdown.RestoreFocusOnClose;
-        		
-        		if (restoreFocusOnCloseFunc is not null)
-        			await restoreFocusOnCloseFunc.Invoke();
-        	}
-        	
-            DropdownService.ReduceClearAction();
-        }
-    }
-    /* End DropdownInitializer */
-    
-    
-    
-    /* Start OutlineInitializer */
-    public partial class OutlineInitializer : ComponentBase, IDisposable
-    {
-    	public string GetStyleCssLeft(OutlineState localOutlineState)
-    	{
-    		var width = OUTLINE_THICKNESS;
-    		
-    		var height = localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels;
-    	
-    		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
-    		
-    		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
-    		
-    		var styleBuilder = new StringBuilder();
-    		
-    		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
-    		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
-    		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
-    		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
-    		
-    		return styleBuilder.ToString();
-    	}
-    	
-    	public string GetStyleCssRight(OutlineState localOutlineState)
-    	{
-    		var width = OUTLINE_THICKNESS;
-    		
-    		var height = localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels;
-    	
-    		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels +
-    			localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels -
-    			OUTLINE_THICKNESS;
-    		
-    		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
-    			
-    		var styleBuilder = new StringBuilder();
-    		
-    		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
-    		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
-    		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
-    		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
-    		
-    		return styleBuilder.ToString();
-    	}
-    	
-    	public string GetStyleCssTop(OutlineState localOutlineState)
-    	{
-    		var width = localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels;
-    		
-    		var height = OUTLINE_THICKNESS;
-    	
-    		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
-    		
-    		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
-    		
-    		var styleBuilder = new StringBuilder();
-    		
-    		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
-    		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
-    		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
-    		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
-    		
-    		return styleBuilder.ToString();
-    	}
-    	
-    	public string GetStyleCssBottom(OutlineState localOutlineState)
-    	{
-    		var width = localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels;
-    		
-    		var height = OUTLINE_THICKNESS;
-    	
-    		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
-    		
-    		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels +
-    			localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels -
-    			OUTLINE_THICKNESS;
-    			
-    		var styleBuilder = new StringBuilder();
-    		
-    		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
-    		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
-    		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
-    		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
-    		
-    		return styleBuilder.ToString();
-    	}
-    }
-    /* End OutlineInitializer */
-    
-    
-    
-    /* Start TooltipInitializer */
-    public partial class TooltipInitializer : ComponentBase, IDisposable { }
-    /* End TooltipInitializer */
-    
-    
 
+    private void DRAG_DispatchSetDragStateActionOnMouseMove(MouseEventArgs mouseEventArgs)
+    {
+        _throttle.Run(new(isOnMouseMove: true, mouseEventArgs));
+    }
+
+    private void DRAG_DispatchSetDragStateActionOnMouseUp(MouseEventArgs mouseEventArgs)
+    {
+        _throttle.Run(new(isOnMouseMove: false, mouseEventArgs));
+    }
+
+	private string DRAG_GetIsActiveCssClass(IDropzone dropzone)
+	{
+		var onMouseOverDropzoneKey = _onMouseOverDropzone?.DropzoneKey ?? Key<IDropzone>.Empty;
+
+		return onMouseOverDropzoneKey == dropzone.DropzoneKey
+            ? "di_active"
+			: string.Empty;
+	}
+	
+	private Task WIDGET_RemoveWidget()
+    {
+    	WidgetService.SetWidget(null);
+    	return Task.CompletedTask;
+    }
+    
+    private async Task DROPDOWN_ClearActiveKeyList()
+    {
+    	var firstDropdown = DropdownService.GetDropdownState().DropdownList.FirstOrDefault();
+    	
+    	if (firstDropdown is not null)
+    	{
+    		var restoreFocusOnCloseFunc = firstDropdown.RestoreFocusOnClose;
+    		
+    		if (restoreFocusOnCloseFunc is not null)
+    			await restoreFocusOnCloseFunc.Invoke();
+    	}
+    	
+        DropdownService.ReduceClearAction();
+    }
+    
+    public string OUTLINE_GetStyleCssLeft(OutlineState localOutlineState)
+	{
+		var width = OUTLINE_THICKNESS;
+		
+		var height = localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels;
+	
+		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
+		
+		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
+		
+		var styleBuilder = new StringBuilder();
+		
+		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
+		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
+		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
+		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
+		
+		return styleBuilder.ToString();
+	}
+	
+	public string OUTLINE_GetStyleCssRight(OutlineState localOutlineState)
+	{
+		var width = OUTLINE_THICKNESS;
+		
+		var height = localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels;
+	
+		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels +
+			localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels -
+			OUTLINE_THICKNESS;
+		
+		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
+			
+		var styleBuilder = new StringBuilder();
+		
+		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
+		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
+		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
+		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
+		
+		return styleBuilder.ToString();
+	}
+	
+	public string OUTLINE_GetStyleCssTop(OutlineState localOutlineState)
+	{
+		var width = localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels;
+		
+		var height = OUTLINE_THICKNESS;
+	
+		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
+		
+		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels;
+		
+		var styleBuilder = new StringBuilder();
+		
+		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
+		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
+		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
+		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
+		
+		return styleBuilder.ToString();
+	}
+	
+	public string OUTLINE_GetStyleCssBottom(OutlineState localOutlineState)
+	{
+		var width = localOutlineState.MeasuredHtmlElementDimensions.WidthInPixels;
+		
+		var height = OUTLINE_THICKNESS;
+	
+		var left = localOutlineState.MeasuredHtmlElementDimensions.LeftInPixels;
+		
+		var top = localOutlineState.MeasuredHtmlElementDimensions.TopInPixels +
+			localOutlineState.MeasuredHtmlElementDimensions.HeightInPixels -
+			OUTLINE_THICKNESS;
+			
+		var styleBuilder = new StringBuilder();
+		
+		styleBuilder.Append($"width: {width.ToCssValue()}px; ");
+		styleBuilder.Append($"height: {height.ToCssValue()}px; ");
+		styleBuilder.Append($"left: {left.ToCssValue()}px; ");
+		styleBuilder.Append($"top: {top.ToCssValue()}px; ");
+		
+		return styleBuilder.ToString();
+	}
+    /* End of misc */
 }
