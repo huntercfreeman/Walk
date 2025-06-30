b4d9073940f1f6c35589c52d878719e6217f421c
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-06-30 16:03:02 -0400

    D.N.B.: ICommonUiService

--- Source/Lib/Common/Dynamics/Models/ICommonUiService.cs
+++ Source/Lib/Common/Dynamics/Models/ICommonUiService.cs
@@ -47,158 +47,137 @@ namespace Walk.Common.RazorLib.Dynamics.Models;
 public interface ICommonUiService
 {
     /* Start IOutlineService */
-    public interface IOutlineService
-    {
-    	public event Action? OutlineStateChanged;
-    	
-    	public OutlineState GetOutlineState();
-    
-    	public void SetOutline(
-    		string? elementId,
-    		MeasuredHtmlElementDimensions? measuredHtmlElementDimensions,
-    		bool needsMeasured);
-    	
-    	/// <summary>
-    	/// The element which was measured is included in order to "handshake" that
-    	/// the element being outlined did not change out from under us.
-    	///
-    	/// If the element did happen to change out from under us, then this action
-    	/// will not do anything.
-    	/// </summary>
-    	public void SetMeasurements(
-    		string? elementId,
-    		MeasuredHtmlElementDimensions? measuredHtmlElementDimensions);
-    }
+	public event Action? Outline_OutlineStateChanged;
+	
+	public OutlineState Outline_GetOutlineState();
+
+	public void Outline_SetOutline(
+		string? elementId,
+		MeasuredHtmlElementDimensions? measuredHtmlElementDimensions,
+		bool needsMeasured);
+	
+	/// <summary>
+	/// The element which was measured is included in order to "handshake" that
+	/// the element being outlined did not change out from under us.
+	///
+	/// If the element did happen to change out from under us, then this action
+	/// will not do anything.
+	/// </summary>
+	public void Outline_SetMeasurements(
+		string? elementId,
+		MeasuredHtmlElementDimensions? measuredHtmlElementDimensions);
     /* End IOutlineService */
     
     /* Start IPanelService */
-    public interface IPanelService
-    {
-    	public event Action? PanelStateChanged;
-    	
-    	public PanelState GetPanelState();
-    	
-    	public void RegisterPanel(Panel panel);
-        public void DisposePanel(Key<Panel> panelKey);
-        public void RegisterPanelGroup(PanelGroup panelGroup);
-        public void DisposePanelGroup(Key<PanelGroup> panelGroupKey);
-    
-        public void RegisterPanelTab(
-        	Key<PanelGroup> panelGroupKey,
-        	IPanelTab panelTab,
-        	bool insertAtIndexZero);
-    
-        public void DisposePanelTab(Key<PanelGroup> panelGroupKey, Key<Panel> panelTabKey);
-        public void SetActivePanelTab(Key<PanelGroup> panelGroupKey, Key<Panel> panelTabKey);
-        public void SetPanelTabAsActiveByContextRecordKey(Key<ContextRecord> contextRecordKey);
-        public void SetDragEventArgs((IPanelTab PanelTab, PanelGroup PanelGroup)? dragEventArgs);
-        public void InitializeResizeHandleDimensionUnit(Key<PanelGroup> panelGroupKey, DimensionUnit dimensionUnit);
-    }
+	public event Action? Panel_PanelStateChanged;
+	
+	public PanelState Panel_GetPanelState();
+	
+	public void Panel_RegisterPanel(Panel panel);
+    public void Panel_DisposePanel(Key<Panel> panelKey);
+    public void Panel_RegisterPanelGroup(PanelGroup panelGroup);
+    public void Panel_DisposePanelGroup(Key<PanelGroup> panelGroupKey);
+
+    public void Panel_RegisterPanelTab(
+    	Key<PanelGroup> panelGroupKey,
+    	IPanelTab panelTab,
+    	bool insertAtIndexZero);
+
+    public void Panel_DisposePanelTab(Key<PanelGroup> panelGroupKey, Key<Panel> panelTabKey);
+    public void Panel_SetActivePanelTab(Key<PanelGroup> panelGroupKey, Key<Panel> panelTabKey);
+    public void Panel_SetPanelTabAsActiveByContextRecordKey(Key<ContextRecord> contextRecordKey);
+    public void Panel_SetDragEventArgs((IPanelTab PanelTab, PanelGroup PanelGroup)? dragEventArgs);
+    public void Panel_InitializeResizeHandleDimensionUnit(Key<PanelGroup> panelGroupKey, DimensionUnit dimensionUnit);
     /* End IPanelService */
 
     /* Start IWidgetService */
-    public interface IWidgetService
-    {
-    	public event Action? WidgetStateChanged;
-    	
-    	public WidgetState GetWidgetState();
-    
-    	/// <summary>
-    	/// When this action causes the transition from a widget being rendered,
-    	/// to NO widget being rendered.
-    	///
-    	/// Then the user's focus will go "somewhere" so we want
-    	/// redirect it to the main layout at least so they can use IDE keybinds
-    	///
-    	/// As if the "somewhere" their focus moves to is outside the blazor app components
-    	/// they IDE keybinds won't fire.
-    	///
-    	/// TODO: Where does focus go when you delete the element which the user is focused on.
-    	///
-    	/// TODO: Prior to focusing the widget (i.e.: NO widget transitions to a widget being rendered)
-    	///           we should track where the user's focus is, then restore that focus once the
-    	///           widget is closed.
-    	/// </summary>
-        public void SetWidget(WidgetModel? widget);
-    }
+	public event Action? Widget_WidgetStateChanged;
+	
+	public WidgetState Widget_GetWidgetState();
+
+	/// <summary>
+	/// When this action causes the transition from a widget being rendered,
+	/// to NO widget being rendered.
+	///
+	/// Then the user's focus will go "somewhere" so we want
+	/// redirect it to the main layout at least so they can use IDE keybinds
+	///
+	/// As if the "somewhere" their focus moves to is outside the blazor app components
+	/// they IDE keybinds won't fire.
+	///
+	/// TODO: Where does focus go when you delete the element which the user is focused on.
+	///
+	/// TODO: Prior to focusing the widget (i.e.: NO widget transitions to a widget being rendered)
+	///           we should track where the user's focus is, then restore that focus once the
+	///           widget is closed.
+	/// </summary>
+    public void Widget_SetWidget(WidgetModel? widget);
     /* End IWidgetService */
     
     /* Start IDialogService */
-    public interface IDialogService
-    {
-    	public event Action? DialogStateChanged;
-    	public event Action? ActiveDialogKeyChanged;
-    	
-    	/// <summary>
-    	/// Capture the reference and re-use it,
-    	/// because the state will change out from under you, if you continually invoke this.
-    	/// </summary>
-    	public DialogState GetDialogState();
+	public event Action? Dialog_DialogStateChanged;
+	public event Action? Dialog_ActiveDialogKeyChanged;
+	
+	/// <summary>
+	/// Capture the reference and re-use it,
+	/// because the state will change out from under you, if you continually invoke this.
+	/// </summary>
+	public DialogState Dialog_GetDialogState();
+
+    public void Dialog_ReduceRegisterAction(IDialog dialog);
+
+    public void Dialog_ReduceSetIsMaximizedAction(
+        Key<IDynamicViewModel> dynamicViewModelKey,
+        bool isMaximized);
     
-        public void ReduceRegisterAction(IDialog dialog);
-    
-        public void ReduceSetIsMaximizedAction(
-            Key<IDynamicViewModel> dynamicViewModelKey,
-            bool isMaximized);
-        
-        public void ReduceSetActiveDialogKeyAction(Key<IDynamicViewModel> dynamicViewModelKey);
-        public void ReduceDisposeAction(Key<IDynamicViewModel> dynamicViewModelKey);
-    }
+    public void Dialog_ReduceSetActiveDialogKeyAction(Key<IDynamicViewModel> dynamicViewModelKey);
+    public void Dialog_ReduceDisposeAction(Key<IDynamicViewModel> dynamicViewModelKey);
     /* End IDialogService */
     
     /* Start INotificationService */
-    public interface INotificationService
-    {
-    	public event Action? NotificationStateChanged;
-    	
-    	public NotificationState GetNotificationState();
-    
-        public void ReduceRegisterAction(INotification notification);
-        public void ReduceDisposeAction(Key<IDynamicViewModel> key);
-        public void ReduceMakeReadAction(Key<IDynamicViewModel> key);
-        public void ReduceUndoMakeReadAction(Key<IDynamicViewModel> key);
-        public void ReduceMakeDeletedAction(Key<IDynamicViewModel> key);
-        public void ReduceUndoMakeDeletedAction(Key<IDynamicViewModel> key);
-        public void ReduceMakeArchivedAction(Key<IDynamicViewModel> key);
-        public void ReduceUndoMakeArchivedAction(Key<IDynamicViewModel> key);
-        public void ReduceClearDefaultAction();
-        public void ReduceClearReadAction();
-        public void ReduceClearDeletedAction();
-        public void ReduceClearArchivedAction();
-    }
+	public event Action? Notification_NotificationStateChanged;
+	
+	public NotificationState Notification_GetNotificationState();
+
+    public void Notification_ReduceRegisterAction(INotification notification);
+    public void Notification_ReduceDisposeAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceMakeReadAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceUndoMakeReadAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceMakeDeletedAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceUndoMakeDeletedAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceMakeArchivedAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceUndoMakeArchivedAction(Key<IDynamicViewModel> key);
+    public void Notification_ReduceClearDefaultAction();
+    public void Notification_ReduceClearReadAction();
+    public void Notification_ReduceClearDeletedAction();
+    public void Notification_ReduceClearArchivedAction();
     /* End INotificationService */
     
     /* Start IDropdownService */
-    public interface IDropdownService
-    {
-    	public event Action? DropdownStateChanged;
-    	
-    	public DropdownState GetDropdownState();
-    	
-        public void ReduceRegisterAction(DropdownRecord dropdown);
-        public void ReduceDisposeAction(Key<DropdownRecord> key);
-        public void ReduceClearAction();
-        public void ReduceFitOnScreenAction(DropdownRecord dropdown);
-    }
+	public event Action? Dropdown_DropdownStateChanged;
+	
+	public DropdownState Dropdown_GetDropdownState();
+	
+    public void Dropdown_ReduceRegisterAction(DropdownRecord dropdown);
+    public void Dropdown_ReduceDisposeAction(Key<DropdownRecord> key);
+    public void Dropdown_ReduceClearAction();
+    public void Dropdown_ReduceFitOnScreenAction(DropdownRecord dropdown);
     /* End IDropdownService */
     
     /* Start ITooltipService */
-    public interface ITooltipService
-    {
-    	public string HtmlElementId { get; }
-    	public MeasuredHtmlElementDimensions HtmlElementDimensions { get; set; }
-    	public MeasuredHtmlElementDimensions GlobalHtmlElementDimensions { get; set; }
-    	public bool IsOffScreenHorizontally { get; }
-    	public bool IsOffScreenVertically { get; }
-    	public int RenderCount { get; }
-    	
-    	public StringBuilder StyleBuilder { get; }
-    
-    	public event Action? TooltipStateChanged;
-    	
-    	public TooltipState GetTooltipState();
-    	
-    	public void SetTooltipModel(ITooltipModel tooltipModel);
-    }
+	public string Tooltip_HtmlElementId { get; }
+	public MeasuredHtmlElementDimensions Tooltip_HtmlElementDimensions { get; set; }
+	public MeasuredHtmlElementDimensions Tooltip_GlobalHtmlElementDimensions { get; set; }
+	public bool Tooltip_IsOffScreenHorizontally { get; }
+	public bool Tooltip_IsOffScreenVertically { get; }
+	public int Tooltip_RenderCount { get; }
+	
+	public StringBuilder Tooltip_StyleBuilder { get; }
+
+	public event Action? Tooltip_TooltipStateChanged;
+	
+	public TooltipState Tooltip_GetTooltipState();
+	
+	public void Tooltip_SetTooltipModel(ITooltipModel tooltipModel);
     /* End ITooltipService */
 }
