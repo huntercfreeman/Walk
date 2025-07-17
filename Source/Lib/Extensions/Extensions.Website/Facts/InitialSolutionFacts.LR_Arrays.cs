a1f2b86ef18b79828c8f4ff73ab75cd779d6fa86
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-07-17 15:13:45 -0400

    Change demo text

--- Source/Lib/Extensions/Extensions.Website/Facts/InitialSolutionFacts.LR_Arrays.cs
+++ Source/Lib/Extensions/Extensions.Website/Facts/InitialSolutionFacts.LR_Arrays.cs
@@ -5,385 +5,215 @@ public partial class InitialSolutionFacts
     public const string PERSON_CS_ABSOLUTE_FILE_PATH = @"/BlazorCrudApp/BlazorCrudApp.Wasm/Persons/Person.cs";
     public const string PERSON_CS_CONTENTS =
 """""""""
-namespace Walk.CompilerServices.CSharp;
+using Microsoft.AspNetCore.Components;
 
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/switch-expression
-public static class SwitchExample
+namespace Walk.Website.RazorLib;
+
+public partial class WalkWebsiteInitializer : ComponentBase
 {
-    public enum Direction
+    [Inject]
+    private ITextEditorRegistryWrap TextEditorRegistryWrap { get; set; } = null!;
+    [Inject]
+    private IDecorationMapperRegistry DecorationMapperRegistry { get; set; } = null!;
+    [Inject]
+    private ICompilerServiceRegistry CompilerServiceRegistry { get; set; } = null!;
+    [Inject]
+    private TextEditorService TextEditorService { get; set; } = null!;
+    [Inject]
+    private ITextEditorHeaderRegistry TextEditorHeaderRegistry { get; set; } = null!;
+    [Inject]
+    private DotNetBackgroundTaskApi DotNetBackgroundTaskApi { get; set; } = null!;
+    [Inject]
+    private CommonUtilityService CommonUtilityService { get; set; } = null!;
+
+    protected override void OnInitialized()
     {
-        Up,
-        Down,
-        Right,
-        Left
+        TextEditorRegistryWrap.DecorationMapperRegistry = DecorationMapperRegistry;
+        TextEditorRegistryWrap.CompilerServiceRegistry = CompilerServiceRegistry;
     }
 
-    public enum Orientation
+    protected override Task OnAfterRenderAsync(bool firstRender)
     {
-        North,
-        South,
-        East,
-        West
-    }
-
-    public static Orientation ToOrientation(Direction direction) => direction switch
-    {
-        Direction.Up    => Orientation.North,
-        Direction.Right => Orientation.East,
-        Direction.Down  => Orientation.South,
-        Direction.Left  => Orientation.West,
-        _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}"),
-    };
-
-    public static void Main()
-    {
-        var direction = Direction.Right;
-        Console.WriteLine($"Map view direction is {direction}");
-        Console.WriteLine($"Cardinal orientation is {ToOrientation(direction)}");
-        // Output:
-        // Map view direction is Right
-        // Cardinal orientation is East
-    }
-}
-
-public readonly struct Point
-{
-    public Point(int x, int y) => (X, Y) = (x, y);
-    
-    public int X { get; }
-    public int Y { get; }
-}
-
-static Point Transform(Point point) => point switch
-{
-    { X: 0, Y: 0 }                    => new Point(0, 0),
-    { X: var x, Y: var y } when x < y => new Point(x + y, y),
-    { X: var x, Y: var y } when x > y => new Point(x - y, y),
-    { X: var x, Y: var y }            => new Point(2 * x, 2 * y),
-};
-
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements#the-switch-statement
-DisplayMeasurement(-4);  // Output: Measured value is -4; too low.
-DisplayMeasurement(5);  // Output: Measured value is 5.
-DisplayMeasurement(30);  // Output: Measured value is 30; too high.
-DisplayMeasurement(double.NaN);  // Output: Failed measurement.
-
-void DisplayMeasurement(double measurement)
-{
-    switch (measurement)
-    {
-        case < 0.0:
-            Console.WriteLine($"Measured value is {measurement}; too low.");
-            break;
-
-        case > 15.0:
-            Console.WriteLine($"Measured value is {measurement}; too high.");
-            break;
-
-        case double.NaN:
-            Console.WriteLine("Failed measurement.");
-            break;
-
-        default:
-            Console.WriteLine($"Measured value is {measurement}.");
-            break;
-    }
-}
-
-DisplayMeasurement(-4);  // Output: Measured value is -4; out of an acceptable range.
-DisplayMeasurement(50);  // Output: Measured value is 50.
-DisplayMeasurement(132);  // Output: Measured value is 132; out of an acceptable range.
-
-void DisplayMeasurement(int measurement)
-{
-    switch (measurement)
-    {
-        case < 0:
-        case > 100:
-            Console.WriteLine($"Measured value is {measurement}; out of an acceptable range.");
-            break;
+        if (firstRender)
+        {
+        	CommonUtilityService.Continuous_EnqueueGroup(new BackgroundTask(
+        		Key<IBackgroundTaskGroup>.Empty,
+        		Do_WalkWebsiteInitializerOnAfterRenderAsync));
+        }
         
-        default:
-            Console.WriteLine($"Measured value is {measurement}.");
-            break;
-    }
-}
-
-DisplayMeasurements(3, 4);  // Output: First measurement is 3, second measurement is 4.
-DisplayMeasurements(5, 5);  // Output: Both measurements are valid and equal to 5.
-
-void DisplayMeasurements(int a, int b)
-{
-    switch ((a, b))
-    {
-        case (> 0, > 0) when a == b:
-            Console.WriteLine($"Both measurements are valid and equal to {a}.");
-            break;
-
-        case (> 0, > 0):
-            Console.WriteLine($"First measurement is {a}, second measurement is {b}.");
-            break;
-
-        default:
-            Console.WriteLine("One or both measurements are not valid.");
-            break;
-    }
-}
-
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/arrays
-
-// Wrapping with a code block will show me if the array syntax breaks code block parsing.
-public void LR_Arrays_A()
-{
-    // # Arrays
-    // 12/14/2024
-    
-    // In this article
-    //    Single-dimensional arrays
-    //    Multidimensional arrays
-    //    Jagged arrays
-    //    Implicitly typed arrays
-    
-    type[] arrayName;
-    
-    type?[] arrayName;
-    type[]? arrayName;
-    type?[]? arrayName;
-    
-    int[] numbers = new int[10];
-    string[] messages = new string[10];
-    
-    int[] array1 = new int[5];
-    
-    int[] array2 = [1, 2, 3, 4, 5, 6];
-    
-    int[,] multiDimensionalArray1 = new int[2, 3];
-    
-    int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
-    
-    int[][] jaggedArray = new int[6][];
-    
-    jaggedArray[0] = [1, 2, 3, 4];
-    
-    int[] array = [1, 2, 3, 4, 5, 6];
-    int[] array2 = {1, 2, 3, 4, 5, 6};
-    
-    int[] array = new int[5];
-    string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    Console.WriteLine(weekDays[0]);
-    Console.WriteLine(weekDays[1]);
-}
-
-class ArrayExample
-{
-    static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
-
-    static void ChangeArray(string[] arr) => Array.Reverse(arr);
-
-    static void ChangeArrayElements(string[] arr)
-    {
-        arr[0] = "Mon";
-        arr[1] = "Wed";
-        arr[2] = "Fri";
-    }
-
-    static void Main()
-    {
-        string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        DisplayArray(weekDays);
-        Console.WriteLine();
-
-        ChangeArray(weekDays);
-        Console.WriteLine("Array weekDays after the call to ChangeArray:");
-        DisplayArray(weekDays);
-        Console.WriteLine();
-
-        ChangeArrayElements(weekDays);
-        Console.WriteLine("Array weekDays after the call to ChangeArrayElements:");
-        DisplayArray(weekDays);
-    }
-}
-
-// Wrapping with a code block will show me if the array syntax breaks code block parsing.
-public void LR_Arrays_B()
-{
-    int[,] array2DDeclaration = new int[4, 2];
-    
-    int[,,] array3DDeclaration = new int[4, 2, 3];
-    
-    int[,] array2DInitialization =  { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-    int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4,   5,  6 } },
-                                    { { 7, 8, 9 }, { 10, 11, 12 } } };
-    
-    System.Console.WriteLine(array2DInitialization[0, 0]);
-    System.Console.WriteLine(array2DInitialization[0, 1]);
-    System.Console.WriteLine(array2DInitialization[1, 0]);
-    System.Console.WriteLine(array2DInitialization[1, 1]);
-    
-    System.Console.WriteLine(array2DInitialization[3, 0]);
-    System.Console.WriteLine(array2DInitialization[3, 1]);
-    
-    System.Console.WriteLine(array3D[1, 0, 1]);
-    System.Console.WriteLine(array3D[1, 1, 2]);
-    
-    var allLength = array3D.Length;
-    var total = 1;
-    for (int i = 0; i < array3D.Rank; i++)
-    {
-        total *= array3D.GetLength(i);
-    }
-    System.Console.WriteLine($"{allLength} equals {total}");
-    
-    int[,] numbers2D = { { 9, 99 }, { 3, 33 }, { 5, 55 } };
-    
-    foreach (int i in numbers2D)
-    {
-        System.Console.Write($"{i} ");
+        return Task.CompletedTask;
     }
     
-    int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4,   5,  6 } },
-                            { { 7, 8, 9 }, { 10, 11, 12 } } };
-    foreach (int i in array3D)
+    public async ValueTask Do_WalkWebsiteInitializerOnAfterRenderAsync()
     {
-        System.Console.Write($"{i} ");
-    }
-    
-    int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4,   5,  6 } },
-                            { { 7, 8, 9 }, { 10, 11, 12 } } };
-    
-    for (int i = 0; i < array3D.GetLength(0); i++)
-    {
-        for (int j = 0; j < array3D.GetLength(1); j++)
+        await WriteFileSystemInMemoryAsync().ConfigureAwait(false);
+
+        await ParseSolutionAsync().ConfigureAwait(false);
+
+        // This code block is hacky. I want the Solution Explorer to from the get-go be fully expanded, so the user can see 'Program.cs'
         {
-            for (int k = 0; k < array3D.GetLength(2); k++)
-            {
-                System.Console.Write($"{array3D[i, j, k]} ");
-            }
-            System.Console.WriteLine();
+            CommonUtilityService.TreeView_MoveRight(
+                DotNetSolutionState.TreeViewSolutionExplorerStateKey,
+                false,
+                false);
+
+            CommonUtilityService.TreeView_MoveRight(
+                DotNetSolutionState.TreeViewSolutionExplorerStateKey,
+                false,
+                false);
+
+            CommonUtilityService.TreeView_MoveRight(
+                DotNetSolutionState.TreeViewSolutionExplorerStateKey,
+            false,
+                false);
         }
-        System.Console.WriteLine();
     }
-}
-    
-// Wrapping with a code block will show me if the array syntax breaks code block parsing.
-public void LR_Arrays_C()
-{
-    static void Print2DArray(int[,] arr)
+
+    private async Task WriteFileSystemInMemoryAsync()
     {
-        for (int i = 0; i < arr.GetLength(0); i++)
+        // Create a Blazor Wasm app
+        await WebsiteProjectTemplateFacts.HandleNewCSharpProjectAsync(
+                WebsiteProjectTemplateFacts.BlazorWasmEmptyProjectTemplate.ShortName!,
+                InitialSolutionFacts.BLAZOR_CRUD_APP_WASM_CSPROJ_ABSOLUTE_FILE_PATH,
+                CommonUtilityService)
+            .ConfigureAwait(false);
+
+        await CommonUtilityService.FileSystemProvider.File.WriteAllTextAsync(
+                InitialSolutionFacts.PERSON_CS_ABSOLUTE_FILE_PATH,
+                InitialSolutionFacts.PERSON_CS_CONTENTS)
+            .ConfigureAwait(false);
+
+        await CommonUtilityService.FileSystemProvider.File.WriteAllTextAsync(
+                InitialSolutionFacts.PERSON_DISPLAY_RAZOR_CS_ABSOLUTE_FILE_PATH,
+                InitialSolutionFacts.PERSON_DISPLAY_RAZOR_CS_CONTENTS)
+            .ConfigureAwait(false);
+
+        await CommonUtilityService.FileSystemProvider.File.WriteAllTextAsync(
+                InitialSolutionFacts.PERSON_DISPLAY_RAZOR_ABSOLUTE_FILE_PATH,
+                InitialSolutionFacts.PERSON_DISPLAY_RAZOR_CONTENTS)
+            .ConfigureAwait(false);
+
+        /*await _fileSystemProvider.File.WriteAllTextAsync(
+                InitialSolutionFacts.BLAZOR_CRUD_APP_ALL_C_SHARP_SYNTAX_ABSOLUTE_FILE_PATH,
+                InitialSolutionFacts.BLAZOR_CRUD_APP_ALL_C_SHARP_SYNTAX_CONTENTS)
+            .ConfigureAwait(false);*/
+
+        // ExampleSolution.sln
+        await CommonUtilityService.FileSystemProvider.File.WriteAllTextAsync(
+                InitialSolutionFacts.SLN_ABSOLUTE_FILE_PATH,
+                InitialSolutionFacts.SLN_CONTENTS)
+            .ConfigureAwait(false);
+
+        var solutionAbsolutePath = CommonUtilityService.EnvironmentProvider.AbsolutePathFactory(
+            InitialSolutionFacts.SLN_ABSOLUTE_FILE_PATH,
+            false);
+
+        // This line is also in WalkExtensionsDotNetInitializer,
+        // but its duplicated here because the website
+        // won't open the first file correctly without this.
+        TextEditorHeaderRegistry.UpsertHeader("cs", typeof(Walk.Extensions.CompilerServices.Displays.TextEditorCompilerServiceHeaderDisplay));
+
+        DotNetBackgroundTaskApi.Enqueue(new DotNetBackgroundTaskApiWorkArgs
         {
-            for (int j = 0; j < arr.GetLength(1); j++)
+        	WorkKind = DotNetBackgroundTaskApiWorkKind.SetDotNetSolution,
+        	DotNetSolutionAbsolutePath = solutionAbsolutePath,
+    	});
+    }
+
+    private async Task ParseSolutionAsync()
+    {
+        var allFiles = new List<string>();
+
+        await RecursiveStep(
+                new List<string> { "/" },
+        allFiles)
+        .ConfigureAwait(false);
+
+        async Task RecursiveStep(IEnumerable<string> directories, List<string> allFiles)
+        {
+            foreach (var directory in directories)
             {
-                System.Console.WriteLine($"Element({i},{j})={arr[i,j]}");
+                var childDirectories = await CommonUtilityService.FileSystemProvider.Directory
+                    .GetDirectoriesAsync(directory)
+                    .ConfigureAwait(false);
+                allFiles.AddRange(
+                    await CommonUtilityService.FileSystemProvider.Directory.GetFilesAsync(directory).ConfigureAwait(false));
+
+                await RecursiveStep(childDirectories, allFiles).ConfigureAwait(false);
             }
         }
-    }
-    static void ExampleUsage()
-    {
-        Print2DArray(new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } });
-    }
-    
-    int[][] jaggedArray = new int[3][];
-    
-    jaggedArray[0] = [1, 3, 5, 7, 9];
-    jaggedArray[1] = [0, 2, 4, 6];
-    jaggedArray[2] = [11, 22];
-    
-    int[][] jaggedArray2 =
-    [
-        [1, 3, 5, 7, 9],
-        [0, 2, 4, 6],
-        [11, 22]
-    ];
-    
-    jaggedArray2[0][1] = 77;
-    
-    jaggedArray2[2][1] = 88;
-    
-    int[][,] jaggedArray3 =
-    [
-        new int[,] { {1,3}, {5,7} },
-        new int[,] { {0,2}, {4,6}, {8,10} },
-        new int[,] { {11,22}, {99,88}, {0,9} }
-    ];
-    
-    Console.Write("{0}", jaggedArray3[0][1, 0]);
-    Console.WriteLine(jaggedArray3.Length);
-    
-    int[][] arr = new int[2][];
-    
-    arr[0] = [1, 3, 5, 7, 9];
-    arr[1] = [2, 4, 6, 8];
-    
-    for (int i = 0; i < arr.Length; i++)
-    {
-        System.Console.Write("Element({0}): ", i);
-    
-        for (int j = 0; j < arr[i].Length; j++)
+
+        foreach (var file in allFiles)
         {
-            System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+            var absolutePath = CommonUtilityService.EnvironmentProvider.AbsolutePathFactory(file, false);
+            var resourceUri = new ResourceUri(file);
+            var fileLastWriteTime = await CommonUtilityService.FileSystemProvider.File.GetLastWriteTimeAsync(file).ConfigureAwait(false);
+            var content = await CommonUtilityService.FileSystemProvider.File.ReadAllTextAsync(file).ConfigureAwait(false);
+
+            var decorationMapper = DecorationMapperRegistry.GetDecorationMapper(absolutePath.ExtensionNoPeriod);
+            var compilerService = CompilerServiceRegistry.GetCompilerService(absolutePath.ExtensionNoPeriod);
+
+            var textEditorModel = new TextEditorModel(
+                resourceUri,
+                fileLastWriteTime,
+                absolutePath.ExtensionNoPeriod,
+                content,
+                decorationMapper,
+                compilerService,
+                TextEditorService);
+
+            TextEditorService.WorkerArbitrary.PostUnique(editContext =>
+            {
+            	TextEditorService.ModelApi.RegisterCustom(editContext, textEditorModel);
+            	
+                var modelModifier = editContext.GetModelModifier(textEditorModel.PersistentState.ResourceUri);
+
+                if (modelModifier is null)
+                    return ValueTask.CompletedTask;
+
+                TextEditorService.ModelApi.AddPresentationModel(
+                    editContext,
+                    modelModifier,
+                    CompilerServiceDiagnosticPresentationFacts.EmptyPresentationModel);
+
+                TextEditorService.ModelApi.AddPresentationModel(
+                    editContext,
+                    modelModifier,
+                    FindOverlayPresentationFacts.EmptyPresentationModel);
+
+                TextEditorService.ModelApi.AddPresentationModel(
+                    editContext,
+                    modelModifier,
+                    DiffPresentationFacts.EmptyInPresentationModel);
+
+                TextEditorService.ModelApi.AddPresentationModel(
+                    editContext,
+                    modelModifier,
+                    DiffPresentationFacts.EmptyOutPresentationModel);
+
+                textEditorModel.PersistentState.CompilerService.RegisterResource(
+                    textEditorModel.PersistentState.ResourceUri,
+                    shouldTriggerResourceWasModified: true);
+                return ValueTask.CompletedTask;
+            });
         }
-        System.Console.WriteLine();
+        
+		TextEditorService.WorkerArbitrary.PostUnique(async editContext =>
+		{
+			// Display a file from the get-go so the user is less confused on what the website is.
+	        var absolutePath = CommonUtilityService.EnvironmentProvider.AbsolutePathFactory(
+	            InitialSolutionFacts.PERSON_CS_ABSOLUTE_FILE_PATH,
+	            false);
+		
+			await TextEditorService.OpenInEditorAsync(
+				editContext,
+	            absolutePath.Value,
+	            false,
+	            null,
+	            new Category("main"),
+	        	Key<TextEditorViewModel>.NewKey());
+		});
     }
-    
-    int[] a = new[] { 1, 10, 100, 1000 };
-    
-    Console.WriteLine("First element: " + a[0]);
-    Console.WriteLine("Second element: " + a[1]);
-    
-    var b = new[] { "hello", null, "world" };
-    
-    Console.WriteLine(string.Join(" ", b));
-    
-    int[][] c =
-    [
-        [1,2,3,4],
-        [5,6,7,8]
-    ];
-    for (int k = 0; k < c.Length; k++)
-    {
-        for (int j = 0; j < c[k].Length; j++)
-        {
-            Console.WriteLine($"Element at c[{k}][{j}] is: {c[k][j]}");
-        }
-    }
-    
-    string[][] d =
-    [
-        ["Luca", "Mads", "Luke", "Dinesh"],
-        ["Karen", "Suma", "Frances"]
-    ];
-    
-    int i = 0;
-    foreach (var subArray in d)
-    {
-        int j = 0;
-        foreach (var element in subArray)
-        {
-            Console.WriteLine($"Element at d[{i}][{j}] is: {element}");
-            j++;
-        }
-        i++;
-    }
-    
-    var contacts = new[]
-    {
-        new
-        {
-            Name = "Eugene Zabokritski",
-            PhoneNumbers = new[] { "206-555-0108", "425-555-0001" }
-        },
-        new
-        {
-            Name = "Hanying Feng",
-            PhoneNumbers = new[] { "650-555-0199" }
-        }
-    };
 }
-
-
-
-
-
 """"""""";
 }
