009b752b2ba9e4dc028fad9eb7cb9b62cbf4d314
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-08-14 09:53:58 -0400

    widget vs dialog

--- Source/Lib/Common/Widgets/Models/SimpleWidgetKind.cs
+++ Source/Lib/Common/Widgets/Models/SimpleWidgetKind.cs
@@ -14,7 +14,17 @@ namespace Walk.Common.RazorLib.Widgets.Models;
 /// display anything you want in a DynamicComponent.
 ///
 /// I'm still quite put off by the idea of even the widgets/dialogs storing those properties and using DynamicComponent.
-/// But for now, my focus is to just get that logic off the menu options. 
+/// But for now, my focus is to just get that logic off the menu options.
+///
+/// (Widget vs Dialog: I need to find a place to put this explanation but a very short explanation is:
+///
+///  Widget: the widgets take control of the user's focus and are no longer rendered once the user onfocusout events of the widget.
+///  these widgets are fixed position with respect to them floating ontop of the existing UI, but furthermore
+///  you cannot move the widgets nor resize them.
+///
+/// Dialog: the dialogs exist whether the user is focused on them or not.
+/// They are fixed position with respect to them floating ontop of the existing UI.
+/// If marked as resizable, then you can move and resize the dialogs.
 /// </summary>
 public enum SimpleWidgetKind
 {
