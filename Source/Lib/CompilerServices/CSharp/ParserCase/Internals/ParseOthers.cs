03dbddc6bdb605f06618e1344a4e9cc0fa2a86cb
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-08-29 13:09:00 -0400

    1 error

--- Source/Lib/CompilerServices/CSharp/ParserCase/Internals/ParseOthers.cs
+++ Source/Lib/CompilerServices/CSharp/ParserCase/Internals/ParseOthers.cs
@@ -58,7 +58,7 @@ public static class ParseOthers
                 
                     if (isNamespaceStatement)
                     {
-                        parserModel.AddNamespaceToCurrentScope(charIntSum);
+                        parserModel.AddNamespaceToCurrentScope(textSpan);
                     }
                 }
                 
