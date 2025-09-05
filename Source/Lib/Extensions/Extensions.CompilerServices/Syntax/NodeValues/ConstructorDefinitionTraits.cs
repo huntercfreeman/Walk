1b5e2211499ed4bb7191230d98c8747ab5fc25eb
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-09-05 15:50:31 -0400

    DNB;VS;: ConstructorDefinitionTraits

--- Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/ConstructorDefinitionTraits.cs
+++ Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/ConstructorDefinitionTraits.cs
@@ -4,15 +4,33 @@ public struct ConstructorDefinitionTraits
 {
     public ConstructorDefinitionTraits(
         TypeReferenceValue returnTypeReference,
+        SyntaxToken openAngleBracketToken,
+        int indexGenericParameterEntryList,
+        int countGenericParameterEntryList,
+        SyntaxToken openParenthesisToken,
         int indexFunctionArgumentEntryList,
-        int countFunctionArgumentEntryList)
+        int countFunctionArgumentEntryList,
+        SyntaxToken closeParenthesisToken)
     {
         ReturnTypeReference = returnTypeReference;
+        OpenAngleBracketToken = openAngleBracketToken;
+        IndexGenericParameterEntryList = indexGenericParameterEntryList;
+        CountGenericParameterEntryList = countGenericParameterEntryList;
+        OpenParenthesisToken = openParenthesisToken;
         IndexFunctionArgumentEntryList = indexFunctionArgumentEntryList;
         CountFunctionArgumentEntryList = countFunctionArgumentEntryList;
+        CloseParenthesisToken = closeParenthesisToken;
     }
-    
+
     public TypeReferenceValue ReturnTypeReference { get; set; }
+
+    public SyntaxToken OpenAngleBracketToken { get; }
+    public int IndexGenericParameterEntryList { get; set; }
+    public int CountGenericParameterEntryList { get; set; }
+    public SyntaxToken CloseAngleBracketToken { get; private set; }
+
+    public SyntaxToken OpenParenthesisToken { get; set; }
     public int IndexFunctionArgumentEntryList { get; set; }
     public int CountFunctionArgumentEntryList { get; set; }
+    public SyntaxToken CloseParenthesisToken { get; set; }
 }
