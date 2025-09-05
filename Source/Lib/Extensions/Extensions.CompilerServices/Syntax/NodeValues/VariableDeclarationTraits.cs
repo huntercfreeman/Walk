270abb1264d998442b1cb7c9398ad49523a67be9
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-09-05 15:53:58 -0400

    DNB;VS;: VariableDeclarationTraits

--- Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/VariableDeclarationTraits.cs
+++ Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/VariableDeclarationTraits.cs
@@ -1,28 +1,36 @@
 using Walk.Extensions.CompilerServices.Syntax.Enums;
+using Walk.TextEditor.RazorLib.Lexers.Models;
 
 namespace Walk.Extensions.CompilerServices.Syntax.NodeValues;
 
 public struct VariableDeclarationTraits
 {
     public VariableDeclarationTraits(
+        TypeReferenceValue typeReference,
         VariableKind variableKind,
-        TypeReferenceValue resultTypeReference,
-        bool hasSetter,
-        bool hasGetter)
+        bool isInitialized,
+        ResourceUri resourceUri,
+        bool hasGetter,
+        bool hasSetter)
     {
+        TypeReference = typeReference;
         VariableKind = variableKind;
-        ResultTypeReference = resultTypeReference;
-        HasSetter = hasSetter;
+        IsInitialized = isInitialized;
+        ResourceUri = resourceUri;
         HasGetter = hasGetter;
+        HasSetter = hasSetter;
     }
 
+    public TypeReferenceValue TypeReference { get; set; }
+
     public VariableKind VariableKind { get; set; }
-    public TypeReferenceValue ResultTypeReference { get; set; }
-    public bool HasSetter { get; set; }
+    public bool IsInitialized { get; set; }
+    public ResourceUri ResourceUri { get; set; }
     public bool HasGetter { get; set; }
-    
+    public bool HasSetter { get; set; }
+
     public bool IsDefault()
     {
-        return ResultTypeReference.IsDefault();
+        return TypeReference.IsDefault();
     }
 }
