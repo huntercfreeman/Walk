8ee796b0599bf7d141b39069381511e6a4bd7b0e
Luthetus <45454132+huntercfreeman@users.noreply.github.com>
2025-09-05 15:47:40 -0400

    DNB;VS;: FunctionDefinitionTraits

--- Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/FunctionDefinitionTraits.cs
+++ Source/Lib/Extensions/Extensions.CompilerServices/Syntax/NodeValues/FunctionDefinitionTraits.cs
@@ -1,21 +1,48 @@
+using Walk.Extensions.CompilerServices.Syntax.Enums;
+
 namespace Walk.Extensions.CompilerServices.Syntax.NodeValues;
 
 public struct FunctionDefinitionTraits
 {
     public FunctionDefinitionTraits(
         TypeReferenceValue returnTypeReference,
+        AccessModifierKind accessModifierKind,
         SyntaxToken openAngleBracketToken,
+        int indexGenericParameterEntryList,
+        int countGenericParameterEntryList,
+        SyntaxToken closeAngleBracketToken,
+        SyntaxToken openParenthesisToken,
         int indexFunctionArgumentEntryList,
-        int countFunctionArgumentEntryList)
+        int countFunctionArgumentEntryList,
+        SyntaxToken closeParenthesisToken,
+        int indexMethodOverloadDefinition)
     {
         ReturnTypeReference = returnTypeReference;
+        AccessModifierKind = accessModifierKind;
         OpenAngleBracketToken = openAngleBracketToken;
+        IndexGenericParameterEntryList = indexGenericParameterEntryList;
+        CountGenericParameterEntryList = countGenericParameterEntryList;
+        CloseAngleBracketToken = closeAngleBracketToken;
+        OpenParenthesisToken = openParenthesisToken;
         IndexFunctionArgumentEntryList = indexFunctionArgumentEntryList;
         CountFunctionArgumentEntryList = countFunctionArgumentEntryList;
+        CloseParenthesisToken = closeParenthesisToken;
+        IndexMethodOverloadDefinition = indexMethodOverloadDefinition;
     }
-    
+
     public TypeReferenceValue ReturnTypeReference { get; set; }
+
+    public AccessModifierKind AccessModifierKind { get; }
+
     public SyntaxToken OpenAngleBracketToken { get; set; }
+    public int IndexGenericParameterEntryList { get; set; }
+    public int CountGenericParameterEntryList { get; set; }
+    public SyntaxToken CloseAngleBracketToken { get; set; }
+
+    public SyntaxToken OpenParenthesisToken { get; set; }
     public int IndexFunctionArgumentEntryList { get; set; }
     public int CountFunctionArgumentEntryList { get; set; }
+    public SyntaxToken CloseParenthesisToken { get; set; }
+    
+    public int IndexMethodOverloadDefinition { get; set; } = -1;
 }
